09336a8f66e59dc8daa39b664e7928a674ae9159
Denis Kotelev <d.kotelev@MacBook-Pro-Denis.local>
2022-10-08 23:36:39 +0600

    feat: add routes in swagger

--- src/swagger/routes/course.ts
+++ src/swagger/routes/course.ts
@@ -85,3 +85,67 @@
  *                   description: boolean resoonse.
  *                   example: true
 */
+
+/**
+ * @swagger
+ * /api/v1/course/learn/${id}:
+ *   get:
+ *     summary: Получение списка слов для изучения
+ *     responses:
+ *       200:
+ *         description: список слов
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                  moneyForWord: 
+ *                      type: number
+ *                      example: 2
+ *                  words:
+ *                      type: array
+ *                      items:
+ *                          type: object
+ *                          properties:
+ *                              id:
+ *                                  type: number
+ *                              word:
+ *                                  type: string
+ *                              translate:
+ *                                  type: string 
+*/
+
+/**
+ * @swagger
+ * /api/v1/course/learn:
+ *   post:
+ *     summary: Завершить обучение слов
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *                courseId:
+ *                  type: number
+ *                money:
+ *                  type: number
+ *                wordIds:
+ *                  type: array
+ *                  items:
+ *                     type: number
+ *                  
+ *     responses:
+ *       200:
+ *         description: добавление выученных слов
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   description: boolean resoonse.
+ *                   example: true
+*/
